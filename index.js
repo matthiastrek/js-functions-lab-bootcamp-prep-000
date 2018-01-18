37ad39152327baebffd95cd2a05ea1b4a66ac688
Matthew Trenk <matt.trenk@gmail.com>
2018-01-18 17:41:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// write your code below!
+// write your code below
+function happyHolidays() {
+  console.log('Happy holidays!')
+}
+
+function happyHolidaysto(name) {
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayto(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountDown(holiday, days) {
+  
+}
